d159c56926fc61938e50f3557e64021acc5b1a00
alireza <ar.ebrahimi96@gmail.com>
2022-02-11 18:41:43 +0330

    change password

--- webapp/users/urls.py
+++ webapp/users/urls.py
@@ -4,4 +4,5 @@ app_name = 'users'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('password/', views.change_password, name='change_password'),
 ]
--- webapp/users/views.py
+++ webapp/users/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 def register(request):
     if request.method != 'POST':
@@ -18,3 +21,21 @@ def register(request):
             return redirect('learning_logs:index')
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('learning_logs:index')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/change_password.html', {
+        'form': form
+    })
